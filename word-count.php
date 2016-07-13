46abc18c37ca7984f9b6c4bd36abbfb81df32629
amare16 <amarenighatu16@gmail.com>
2016-07-13 23:57:54 +0200

    fourth commit

--- word-count.php
+++ word-count.php
@@ -2,7 +2,7 @@
 
 function wordCount($phrase= "") {
     $phrase= "The str_word_count() function counts the number of words in a string.";
-    return str_word_count(explode($phrase));
+    return str_word_count(explode('', $phrase));
 }
 
 ?>
