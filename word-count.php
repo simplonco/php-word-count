dee76f2cb87e208f944e6894e602f8a0a7b2e912
amare <amare>
2016-07-13 23:49:07 +0200

    third commit

--- word-count.php
+++ word-count.php
@@ -1,8 +1,8 @@
 <?php
 
 function wordCount($phrase= "") {
-    $phrase= "olly olly in come free";
-    return str_word_count($phrase);
+    $phrase= "The str_word_count() function counts the number of words in a string.";
+    return str_word_count(explode($phrase));
 }
 
 ?>
